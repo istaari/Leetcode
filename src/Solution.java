38458663b16e7d7348986b9984fc4f951d258e0a
Roshan Gupta <roshankgupta13@gmail.com>
2025-11-20 23:05:49 +0530

    adding maths book

--- src/Solution.java
+++ src/Solution.java
@@ -1,9 +1,10 @@
 public class Solution {
 
     public static void main(String[] args) {
-        int x = 1;
-        System.out.println(x << 31); // Output: 1 (same as x << 0)
-
+        while (true) {
+            int x = 1;
+            System.out.println(x << 31); // Output: 1 (same as x << 0)
+        }
     }
 
 }
